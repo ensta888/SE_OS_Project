bb9ba87c41187801b9485a9771bfdea4635e960b
nicolas2lee <nicolas2lee@gmail.com>
2014-12-15 22:42:55 +0100

    open correctly, how to calculate the size of image en kilo-oct ? width*height*3?

--- SE_projet/Code/MyCode/CreatBankImage.c
+++ SE_projet/Code/MyCode/CreatBankImage.c
@@ -14,6 +14,7 @@ void insertImageName(ListImage current,){
 }*/
 
 typedef struct _NodeImage{
+	char * path;
 	char  * imageName;
 	int size; //taille du fichier en kilo-octets
 	int height; //height of image
@@ -21,25 +22,40 @@ typedef struct _NodeImage{
 	struct _NodeImage *next;
 }NodeImage;
 
-void createNodeImage(char * imagename){
-	//printf ("input image name is %s\n",imagename);
+void createNodeImage(char * imagename,const char * path){
+	//ajouter le nom complet avec la repertoire
+	char * filename= (char *) malloc(sizeof(char)*255);
+	sprintf(filename,"%s/%s",path,imagename);
+
 	NodeImage * img=(NodeImage *) malloc(sizeof(NodeImage));
-	//printf ("Node Image imageName is %s\n",img->imageName);
+	img->path=(char *) malloc(sizeof(char)*100);
 	img->imageName=(char *) malloc(sizeof(char)*100);
+	img->path=path;
 	img->imageName=imagename;
-	printf ("image Name is %s\n",img->imageName);
+	//printf ("Path is %s\n",img->path);
+	
+
 	//ouvrir l'image avec un type d'targa   la fonction est dans un autre fichier
-	targa_header headImage;//=(targa_header *) malloc(sizeof(targa_header));
-	image_desc imgdesc;//=(image_desc *) malloc(sizeof(image_desc));
+	targa_header headImage;
+	image_desc imgdesc;
 
 	int ans;
-	ans=readImage(&imgdesc,&headImage,imagename);
-	//probleme de nom d'image, car le variable imagename n'a pas de path de repertoire
+	ans=readImage(&imgdesc,&headImage,filename);
+	if (ans!=1){
+		perror("read image failed!");
+		exit(1);  //comment traiter les erreurs ????
+	}
 
-	printf ("ans of reading image is %d\n",ans);
-/*
-	printf("[mem_targa] Header : %u %u %u %u %u\n", headImage.idlength, headImage.colourmaptype, headImage.datatypecode, headImage.width, headImage.height);
-*/
+	img->width=headImage.width;
+	img->height=headImage.height;
+	int size=img->width*img->height*3/1000;
+	img->size=size;
+	printf ("Image Info:\n");
+	printf ("-----------------------\n");
+	printf ("image Name is %s\n",img->imageName);
+	printf("image width is %d, height is %d\n",img->width,img->height);
+	printf ("image size is %d ko\n",img->size);
+	
 }
 
 char* readBankImage(const char *path){
@@ -62,7 +78,7 @@ char* readBankImage(const char *path){
 			}else{
 //Ici a l'instant on n'a pas considere si l'image n'a pas une format targa
 
-				createNodeImage(enter->d_name);
+				createNodeImage(enter->d_name,path);
 				//printf("filename%d=%s\n",i,enter->d_name);//afficher les noms des images
 			}
 			/*
